17b94e40fcd2ba30a6fa27f2916f26eede66f1da
angwanghyeon <agh1594@naver.com>
2023-08-20 23:09:49 +0900

    공지사항 준비

---
+++ src/components/MainBoardList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainBoardList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MainBoardList;
---
+++ src/sections/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+const Header = () => {
+    return (
+        <div>
+           헤더입니다.          
+        </div>
+    );
+};
+
+export default Header;
