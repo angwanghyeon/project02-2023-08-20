7928ed9414e01263370de1d31eddddd69fc26b77
gwanghyeon <agh0625@gmail.com>
2023-08-24 08:50:47 +0900

    커밋 남은거

--- src/realmain/components/OnModal.jsx
+++ src/realmain/components/OnModal.jsx
@@ -8,7 +8,13 @@ const OnModal = ({setModalOpen, id, title, content, writer}) => {
   return (
     <div className='modal-container'>
       <button className='modal-close' onClick={closeModal}>X</button>
-        <p>답변 양식 작성중</p>
+        <div className='answer-writer'>
+          <p className='name'>답변 작성자 이름</p>
+          <p className='date'>작성일자</p>
+        </div>
+        <div className='answer-content'>
+          <textarea cols="30" rows="10" className='answer-inside' placeholder='답변을 작성해주세요'></textarea>
+        </div>
     </div>
   );
 };
